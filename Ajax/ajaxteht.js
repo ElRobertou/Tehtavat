8ccc77bfab624cb319fcbce06af28fbe2eeeb219
ElRobertou <roope.lunden@gmail.com>
2018-11-15 14:11:26 +0200

    Ajax

---
+++ Ajax/ajaxteht.js
@@ -0,0 +1,26 @@
+
+function etsi () {
+  const haku = document.getElementById('haku').value
+  fetch(`https://api.tvmaze.com/search/shows?q=${haku}`)
+  .then(vastaus => vastaus.json())
+  .then(series => {
+    console.log(series);
+    const app = document.getElementById('app');
+    app.innerHTML = series.map(({show}) => `
+            <div class="movies2 col " >
+            ${show.image ? `<img src="${show.image.medium}">` : ' <img style="z-index: -1" src="noimage.png" >'}
+           
+                <div>
+               <h5 >${show.name}</h5>
+                    <h7>${show.url}</h7>
+                    <h7>${show.genres}</h7>
+                    <h7>${show.summary}</h7>
+               </div>
+               </div>
+               
+             `).join('');
+  })
+}
+
+const nappi = document.getElementById('nappi');
+nappi.addEventListener('click', etsi);
